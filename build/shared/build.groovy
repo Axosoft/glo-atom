402f605daa8fd328161ba334dace6f9c0c60c8c6
Unknown <joseg@axosoft.com>
2017-09-27 16:12:39 -0700

    update build files

--- build/shared/build.groovy
+++ build/shared/build.groovy
@@ -1,33 +1,7 @@
-def build_shared_ui(configuration) {
-  // pull shared UI
-  sshagent (credentials: ['axosoft-build']) {
-    // install depencencies
-    sh "git clone git@github.com:Axosoft/tasks-ui.git"
-  
-    dir ('tasks-ui') {
-      sh 'yarn'
-
-      // HACK: build shared styles
-      dir("./node_modules/gitkraken-shared-styles") {
-        sh 'yarn build'
-      }
-
-      // copy the config.json for tasks-ui package before building it
-      sh "cp ./build/${configuration}/config.json config.json"
-
-      // build shared UI
-      sh 'yarn build'
-    }
-  }
-}
-
 def build_atom(configuration) {
   def fileName = "glo-atom-${configuration}.zip"
 
-  sh 'cp tasks-ui/dist/TasksUI.js lib/TasksUI.js'
-  sh 'cp -a tasks-ui/node_modules/gitkraken-shared-styles/icons lib/icons'
-
-  // move all necessary files/folder to 
+  // move all necessary files/folder to
   sh 'mkdir zip'
   sh 'mv keymaps zip/keymaps'
   sh 'mv lib zip/lib'
